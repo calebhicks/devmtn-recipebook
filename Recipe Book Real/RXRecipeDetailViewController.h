1841ecc4f434771196acdac2f5624ab2d0e2b9ac
Caleb Hicks <calebhicks@gmail.com>
2014-05-26 16:13:56 -0600

    late in committing, but it's almost done

---
+++ Recipe Book Real/RXRecipeDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  RXRecipeDetailViewController.h
+//  Recipe Book Real
+//
+//  Created by Caleb Hicks on 5/24/14.
+//  Copyright (c) 2014 Dev Mountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RXRecipeDetailViewController : UIViewController
+
+@end
---
+++ Recipe Book Real/RXRecipeDetailViewController.m
@@ -0,0 +1,49 @@
+//
+//  RXRecipeDetailViewController.m
+//  Recipe Book Real
+//
+//  Created by Caleb Hicks on 5/24/14.
+//  Copyright (c) 2014 Dev Mountain. All rights reserved.
+//
+
+#import "RXRecipeDetailViewController.h"
+
+@interface RXRecipeDetailViewController ()
+
+@end
+
+@implementation RXRecipeDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
